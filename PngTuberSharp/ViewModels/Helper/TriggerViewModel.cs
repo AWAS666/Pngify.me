6f653fe3ac284da9433f4f70a16da4e70ba1b397
AWAS666 <AWAS6@protonmail.com>
2024-08-20 20:32:18 +0200

    feat: hotkey box in layer trigger

--- PngTuberSharp/ViewModels/Helper/TriggerViewModel.cs
+++ PngTuberSharp/ViewModels/Helper/TriggerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 using GlobalHotKeys.Native.Types;
 using PngTuberSharp.Layers;
 using PngTuberSharp.Services.Settings;
@@ -17,6 +18,33 @@ namespace PngTuberSharp.ViewModels.Helper
         [ObservableProperty]
         private Trigger trigger;
 
+        [ObservableProperty]
+        private ObservableCollection<VirtualKeyCode> allKeys;
+
+        [ObservableProperty]
+        private ObservableCollection<Modifiers> allModifiers;
+
+        private string hotkey;
+
+        public string Hotkey
+        {
+            get => hotkey; set
+            {
+                SetProperty(ref hotkey, value);
+                if (!hotkeyByTrigger)
+                {
+                    var hot = (HotkeyTrigger)Trigger;
+                    hot.VirtualKeyCode = 0;
+                    hot.Modifiers = 0;
+                }
+
+                hotkeyByTrigger = false;
+            }
+        }
+
+        bool hotkeyByTrigger;
+
+
         public TriggerViewModel(Trigger trigger)
         {
             Trigger = trigger;
@@ -26,10 +54,29 @@ namespace PngTuberSharp.ViewModels.Helper
             AllModifiers.Insert(0, 0);
         }
 
-        [ObservableProperty]
-        private ObservableCollection<VirtualKeyCode> allKeys;
 
-        [ObservableProperty]
-        private ObservableCollection<Modifiers> allModifiers;
+        public void OnKeyDown(KeyEventArgs e)
+        {
+            if (e?.Key == null)
+                return;
+            e.Handled = true;
+            var hot = (HotkeyTrigger)Trigger;
+            hot.VirtualKeyCode = (VirtualKeyCode)Avalonia.Win32.Input.KeyInterop.VirtualKeyFromKey(e.Key);
+            hot.Modifiers = (Modifiers)e.KeyModifiers;
+            SetHotkey();
+        }
+
+        public void SetHotkey()
+        {
+            string text;
+            var hot = (HotkeyTrigger)Trigger;
+            if (hot.Modifiers == 0)
+                text = $"{hot.VirtualKeyCode}";
+            else
+                text = $"{hot.Modifiers} + {hot.VirtualKeyCode}";
+
+            hotkeyByTrigger = true;
+            Hotkey = text;
+        }
     }
 }
