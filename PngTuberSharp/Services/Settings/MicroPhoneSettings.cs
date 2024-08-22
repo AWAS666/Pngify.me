12c73b60b21262b22942e3bcb31b86380bc6590d
AWAS666 <AWAS6@protonmail.com>
2024-08-22 22:44:56 +0200

    fix: default mic at 0

--- PngTuberSharp/Services/Settings/MicroPhoneSettings.cs
+++ PngTuberSharp/Services/Settings/MicroPhoneSettings.cs
@@ -12,7 +12,7 @@ namespace PngTuberSharp.Services.Settings
     public class MicroPhoneSettings
     {
         public int ThreshHold { get; set; } = 50;
-        public int Device { get; set; } = 1;
+        public int Device { get; set; } = 0;
         public double BlinkInterval { get; set; } = 2f;
         public double BlinkTime { get; set; } = 0.25f;
         public List<MicroPhoneState> States { get; set; } = new List<MicroPhoneState>()
