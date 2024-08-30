00337e8e5932f97701da5ce815b2e1b47beb0eb9
AWAS666 <AWAS6@protonmail.com>
2024-08-30 23:00:11 +0200

    feat: credits

---
+++ PngTuberSharp/Views/CreditsAndInfo.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using Avalonia.Data;
+using PngTuberSharp.Services;
+using PngTuberSharp.ViewModels;
+
+namespace PngTuberSharp.Views;
+
+public partial class CreditsAndInfo : UserControl
+{
+    public CreditsAndInfo()
+    {
+        InitializeComponent();       
+    }
+}
