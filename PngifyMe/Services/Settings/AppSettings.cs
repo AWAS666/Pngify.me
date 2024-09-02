268d0639348eae79f4ca7332257bd450d9cd5c68
AWAS666 <AWAS6@protonmail.com>
2024-09-02 20:09:18 +0200

    refactor: remove basic mic and basic avatar

--- PngifyMe/Services/Settings/AppSettings.cs
+++ PngifyMe/Services/Settings/AppSettings.cs
@@ -7,7 +7,6 @@ namespace PngifyMe.Services.Settings
         public GeneralSettings General { get; set; } = new();
         public MicroPhoneSettings Microphone { get; set; } = new();
         public BackgroundSettings Background { get; set; } = new();
-        public AvatarSettings Avatar { get; set; } = new();
         public LayerSetup LayerSetup { get; set; } = new();
         public TwitchSettings Twitch { get; set; } = new();
         public TitsSettings Tits { get; set; } = new();
