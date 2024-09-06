2139d462144242466bd90fb7952da3df31fc8b38
AWAS666 <AWAS6@protonmail.com>
2024-09-06 22:34:22 +0200

    feat: full twitch redeems

--- PngifyMe/Services/TTSPet/TTSPet.cs
+++ PngifyMe/Services/TTSPet/TTSPet.cs
@@ -16,7 +16,7 @@ namespace PngifyMe.Services.TTSPet
         static TTSPet()
         {
             TwitchEventSocket.BitsUsed += BitsUsed;
-            TwitchEventSocket.RedeemUsed += RedeemUsed;
+            TwitchEventSocket.RedeemFull += RedeemUsed;
             TwitchEventSocket.NewFollower += NewFollower;
         }
 
@@ -25,7 +25,7 @@ namespace PngifyMe.Services.TTSPet
 
         }
 
-        private static void RedeemUsed(object? sender, string e)
+        private static void RedeemUsed(object? sender, ChannelPointsCustomRewardRedemption e)
         {
         }
 
