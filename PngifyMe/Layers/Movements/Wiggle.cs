35586aee7d680c6d22425010f117a4fd6cb997e2
AWAS666 <AWAS6@protonmail.com>
2024-09-07 22:44:49 +0200

    feat: add more layers

--- PngifyMe/Layers/Movements/Wiggle.cs
+++ PngifyMe/Layers/Movements/Wiggle.cs
@@ -5,21 +5,17 @@ using System;
 
 namespace PngifyMe.Layers
 {
-    public class HopOnTalk : RampOnConditionLayer
+    public class Wiggle : MovementBaseLayer
     {
         [Unit("pixels")]
         public float Offset { get; set; } = 20f;
         public float Frequency { get; set; } = 5f;
-        public HopOnTalk()
-        {
-        }
+
         public override void OnCalculateParameters(float dt, ref LayerValues values)
         {
             float pi2 = MathF.PI * 2;
-            values.PosY += (float)Math.Sin(pi2 * CurrentTime * Frequency) * Offset * CurrentStrength 
-                + Offset * CurrentStrength;
+            values.PosY += (float)Math.Sin(pi2 * CurrentTime * Frequency) * Offset * CurrentStrength;
+            values.PosX += (float)Math.Sin(pi2 * CurrentTime * Frequency / 2) * Offset * CurrentStrength;
         }
-
-        public override bool Triggered() => MicrophoneService.Talking;
     }
 }
