6b514e2871e92a41ca5fbf1db27cb807416d5c80
AWAS666 <AWAS6@protonmail.com>
2024-09-15 17:29:49 +0200

    feat: display twitch status

--- PngifyMe/Views/MainWindow.axaml.cs
+++ PngifyMe/Views/MainWindow.axaml.cs
@@ -3,9 +3,13 @@ using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using PngifyMe.Services;
+using PngifyMe.Services.Twitch;
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using TwitchLib.Api.Core.HttpCallHandlers;
 
 namespace PngifyMe.Views;
 
@@ -25,6 +29,17 @@ public partial class MainWindow : Window
         grid.Bind(Grid.BackgroundProperty, binding);
 
         this.Title = $"{Title}-{Assembly.GetExecutingAssembly().GetName().Version?.ToString()}-beta";
+        TwitchEventSocket.Authenticated += Authenticated;
+        if (SettingsManager.Current.Twitch.Enabled == true)
+            twitchStatus.Text = "Connecting";
+    }
+
+    private void Authenticated(object? sender, TwitchAuth e)
+    {
+        Dispatcher.UIThread.Invoke(() =>
+        {
+            twitchStatus.Text = "Connected";
+        });
     }
 
     private void DoubleClick(object? sender, TappedEventArgs e)
@@ -38,6 +53,10 @@ public partial class MainWindow : Window
         hintBox.IsVisible = false;
     }
 
+    private void TwitchPressed(object? sender, PointerPressedEventArgs e)
+    {
+        tabs.SelectedIndex = 7;
+    }   
 
     private void SaveSettings(object? sender, RoutedEventArgs e)
     {
