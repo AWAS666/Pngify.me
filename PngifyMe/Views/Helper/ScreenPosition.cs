333d787d613bc24e88d58e649795ed227774cded
AWAS666 <AWAS6@protonmail.com>
2024-09-23 21:01:29 +0200

    feat: remember last window position

---
+++ PngifyMe/Views/Helper/ScreenPosition.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using PngifyMe.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PngifyMe.Views.Helper
+{
+    public class ScreenPosition
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public WindowState WindowState { get; set; }
+
+        public static void Save(ScreenPosition pos)
+        {
+            var path = Path.Combine(Specsmanager.BasePath, "screen.json");
+            File.WriteAllText(Path.Combine(Specsmanager.BasePath, "screen.json"), JsonSerializer.Serialize(pos));
+        }
+
+        public static ScreenPosition Load()
+        {
+            var path = Path.Combine(Specsmanager.BasePath, "screen.json");
+            if (!File.Exists(path))
+                return null;
+            return JsonSerializer.Deserialize<ScreenPosition>(File.ReadAllText(path));
+        }
+    }
+}
--- PngifyMe/Views/MainWindow.axaml.cs
+++ PngifyMe/Views/MainWindow.axaml.cs
@@ -6,10 +6,13 @@ using Avalonia.Interactivity;
 using Avalonia.Threading;
 using PngifyMe.Services;
 using PngifyMe.Services.Twitch;
+using PngifyMe.Views.Helper;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using TwitchLib.Api.Core.HttpCallHandlers;
+using TwitchLib.Api.ThirdParty.ModLookup;
 
 namespace PngifyMe.Views;
 
@@ -32,6 +35,49 @@ public partial class MainWindow : Window
         TwitchEventSocket.Authenticated += Authenticated;
         if (SettingsManager.Current.Twitch.Enabled == true)
             twitchStatus.Text = "Connecting";
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        ScreenPosition.Save(new ScreenPosition()
+        {
+            Left = Position.X,
+            Top = Position.Y,
+            Width = Width,
+            Height = Height,
+            WindowState = WindowState
+        });
+        base.OnClosing(e);
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        // somehow this offset a little sometimes??
+        var pos = ScreenPosition.Load();
+        if (pos != null)
+        {
+            switch (pos.WindowState)
+            {
+                case WindowState.Normal:
+                    var newPos = new PixelPoint(pos.Left, pos.Top);
+                    // check if on screen 
+                    if (Screens.All.FirstOrDefault(x => x.WorkingArea.Contains(newPos)) == null)
+                        return;
+                    Width = pos.Width;
+                    Height = pos.Height;
+                    break;
+                case WindowState.Minimized:
+                    break;
+                case WindowState.Maximized:
+                    break;
+                case WindowState.FullScreen:
+                    break;
+                default:
+                    break;
+            }
+            WindowState = pos.WindowState;
+        }
+        base.OnLoaded(e);
     }
 
     private void Authenticated(object? sender, TwitchAuth e)
