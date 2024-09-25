173ba33bc7dce5f3fd4f6eb92e5f69605a20921a
AWAS666 <AWAS6@protonmail.com>
2024-09-25 19:22:10 +0200

    feat: deploy script

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=Windows
+dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=Linux
+dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=MacOs
+
+7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-linux.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\linux-x64\"
+7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-win.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\win-x64\"
+7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-osx.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\osx-arm64\"
+
+explorer ".\PngifyMe.Desktop\bin\Release\net8.0\publish\"
