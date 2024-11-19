4083730f5b877f480aae38b176e9899559ad58de
AWAS666 <AWAS6@protonmail.com>
2024-11-19 19:17:38 +0100

    build: bump dependencies

--- deploy.bat
+++ deploy.bat
@@ -2,8 +2,8 @@ dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=Wind
 dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=Linux
 dotnet publish .\PngifyMe.Desktop\PngifyMe.Desktop.csproj -p:PublishProfile=MacOs
 
-7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-linux.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\linux-x64\"
-7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-win.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\win-x64\"
-7z a ".\PngifyMe.Desktop\bin\Release\net8.0\publish\Pngify.Me-osx.zip" ".\PngifyMe.Desktop\bin\Release\net8.0\publish\osx-arm64\"
+7z a ".\PngifyMe.Desktop\bin\Release\net9.0\publish\Pngify.Me-linux.zip" ".\PngifyMe.Desktop\bin\Release\net9.0\publish\linux-x64\"
+7z a ".\PngifyMe.Desktop\bin\Release\net9.0\publish\Pngify.Me-win.zip" ".\PngifyMe.Desktop\bin\Release\net9.0\publish\win-x64\"
+7z a ".\PngifyMe.Desktop\bin\Release\net9.0\publish\Pngify.Me-osx.zip" ".\PngifyMe.Desktop\bin\Release\net9.0\publish\osx-arm64\"
 
-explorer ".\PngifyMe.Desktop\bin\Release\net8.0\publish\"
+explorer ".\PngifyMe.Desktop\bin\Release\net9.0\publish\"
