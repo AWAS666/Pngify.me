d27fba49b60944d19f179761a306e71e774c54d3
AWAS666 <AWAS6@protonmail.com>
2024-12-21 22:40:14 +0100

    feat(layer): single hop

--- PngifyMe/Layers/Movements/HopOnceOnTalk.cs
+++ PngifyMe/Layers/Movements/HopOnceOnTalk.cs
@@ -4,20 +4,20 @@ using System;
 
 namespace PngifyMe.Layers
 {
-    [LayerDescription("Y movement whenever talking")]
-    public class HopOnTalk : RampOnConditionLayer
+    [LayerDescription("hop once when starting to talk")]
+    public class HopOnceOnTalk : RampOnConditionOnceLayer
     {
         [Unit("pixels")]
         public float Offset { get; set; } = 20f;
         public float Frequency { get; set; } = 2f;
-        public HopOnTalk()
+        public HopOnceOnTalk()
         {
         }
         public override void OnCalculateParameters(float dt, ref LayerValues values)
         {
             float pi2 = MathF.PI * 2;
-            values.PosY += (float)Math.Sin(pi2 * CurrentTime * Frequency) *
-                Offset * CurrentStrength
+            values.PosY += (float)Math.Sin(pi2 * CurrentTime * Frequency)
+                * Offset * CurrentStrength
                 - Offset * CurrentStrength;
         }
 
---
+++ PngifyMe/Layers/Movements/RampOnConditionOnceLayer.cs
@@ -0,0 +1,82 @@
+﻿using PngifyMe.Layers.Helper;
+using System;
+
+namespace PngifyMe.Layers
+{
+    public abstract class RampOnConditionOnceLayer : PermaLayer
+    {
+        private float stateChangeTime;
+        private bool lastCondition;
+        private float activatedTime;
+        private bool activating;
+
+        [Unit("s")]
+        public float ActivationRamp { get; set; } = 0.5f;
+
+        [Unit("s")]
+        public float ActiveTime { get; set; } = 0.5f;
+
+        [Unit("s")]
+        public float DeActivationRamp { get; set; } = 0.5f;
+
+        [Unit("s")]
+        public float RemoveTime { get; set; } = float.MaxValue;
+
+        public override void OnEnter()
+        {
+            AutoRemoveTime = RemoveTime;
+            base.OnEnter();
+        }
+
+        public RampOnConditionOnceLayer()
+        {
+            EnterTime = 0f;
+            ExitTime = 0f;
+        }
+
+        public abstract bool Triggered();
+
+        public override void OnUpdate(float dt, float time)
+        {
+            bool current = Triggered();
+            if (!lastCondition && current && !activating)
+            {
+                if (CurrentTime - stateChangeTime < DeActivationRamp)
+                {
+                    var perce = 1 - (CurrentTime - stateChangeTime) / DeActivationRamp;
+                    stateChangeTime = CurrentTime - ActivationRamp * perce;
+                }
+                else
+                    stateChangeTime = CurrentTime;
+                activatedTime = stateChangeTime;
+                activating = true;
+            }
+
+            if (activatedTime + ActivationRamp + ActiveTime < CurrentTime && activating)
+            {
+                if (CurrentTime - stateChangeTime < ActivationRamp)
+                {
+                    var perce = (CurrentTime - stateChangeTime) / DeActivationRamp;
+                    stateChangeTime = CurrentTime - DeActivationRamp * (1 - perce);
+                }
+                else
+                    stateChangeTime = CurrentTime;
+                activating = false;
+            }
+
+            var value = Easings.CubicEaseInOut(Math.Min(1, (CurrentTime - stateChangeTime) / (activating ? ActivationRamp : DeActivationRamp)));
+
+            if (activating)
+                CurrentStrength = value;
+            else
+                CurrentStrength = 1 - value;
+
+            if (stateChangeTime == 0)
+                CurrentStrength = 0;
+
+            lastCondition = current;
+            base.OnUpdate(dt, time);
+        }
+
+    }
+}
