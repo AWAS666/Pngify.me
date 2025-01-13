53eaf89e05d05585afcacd0d01af8750f13f7f12
AWAS666 <AWAS6@protonmail.com>
2025-01-13 18:28:47 +0100

    fix: better float input box

--- PngifyMe/Views/Helper/FloatTextBox.cs
+++ PngifyMe/Views/Helper/FloatTextBox.cs
@@ -2,107 +2,105 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
-namespace PngifyMe.Views.Helper
+namespace PngifyMe.Views.Helper;
+public class FloatTextBox : TextBox
 {
-    public class FloatTextBox : TextBox
+    public static readonly StyledProperty<float> MaxProperty =
+        AvaloniaProperty.Register<FloatTextBox, float>(nameof(Max), defaultValue: float.MaxValue);
+
+    public float Max
     {
-        public static readonly StyledProperty<float> MaxProperty =
-            AvaloniaProperty.Register<UintTextBox, float>(nameof(Max), defaultValue: float.MaxValue);
+        get => GetValue(MaxProperty);
+        set => SetValue(MaxProperty, value);
+    }
 
-        public float Max
+    public static readonly StyledProperty<float> MinProperty =
+        AvaloniaProperty.Register<FloatTextBox, float>(nameof(Min), defaultValue: float.MinValue);
+
+    public float Min
+    {
+        get => GetValue(MinProperty);
+        set => SetValue(MinProperty, value);
+    }
+
+    public FloatTextBox()
+    {
+        this.GetObservable(TextProperty).Subscribe(OnTextChanged);
+    }
+    protected override Type StyleKeyOverride { get { return typeof(TextBox); } }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        if (e.PhysicalKey == PhysicalKey.Backspace)
         {
-            get => GetValue(MaxProperty);
-            set => SetValue(MaxProperty, value);
-        }
-
-        public static readonly StyledProperty<float> MinProperty =
-            AvaloniaProperty.Register<UintTextBox, float>(nameof(Min), defaultValue: float.MinValue);
-        private bool inputHandler;
-
-        public float Min
-        {
-            get => GetValue(MinProperty);
-            set => SetValue(MinProperty, value);
-        }
-
-        private bool handling;
-
-        public FloatTextBox()
-        {
-            this.GetObservable(TextProperty).Subscribe(OnTextChanged);
-        }
-        protected override Type StyleKeyOverride { get { return typeof(TextBox); } }
-
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            if (e.PhysicalKey == PhysicalKey.Backspace)
+            if (Text.Length <= 1)
             {
-                if (Text.Length <= 1)
-                {
-                    Text = "0.0";
-                    e.Handled = true;
-                    return;
-                }
-            }
-            base.OnKeyDown(e);
-
-            if (string.IsNullOrEmpty(Text))
-                Text = "0.0";
-
-            float newValue = float.Parse(Text);
-            float? fixedVal = CheckBounds(newValue);
-            if (fixedVal != null)
-                SetValue((float)fixedVal);
-        }
-
-        protected override void OnTextInput(TextInputEventArgs e)
-        {
-            // Regex to allow only numeric input
-            if (!Regex.IsMatch(e.Text, @"^[0-9]*$")
-                || !float.TryParse(Text + e.Text, out var num)
-                )
-            {
+                Text = 0.0f.ToString();
                 e.Handled = true;
-            }
-            base.OnTextInput(e);
-
-            float newValue = float.Parse(Text);
-            float? fixedVal = CheckBounds(newValue);
-            if (fixedVal != null)
-                SetValue((float)fixedVal);
-        }
-
-        private void SetValue(float fixedVal)
-        {
-            string commaSeparator = ".";
-            string result = fixedVal.ToString();
-            if (!result.Contains(commaSeparator))
-            {
-                result += commaSeparator + "0";
-            }
-            Text = result;
-        }
-
-        private float? CheckBounds(float newValue)
-        {
-            if (newValue > Max)
-                return Max;
-            if (newValue < Min)
-                return Min;
-            return null;
-        }
-
-        private void OnTextChanged(string newValue)
-        {
-            if (string.IsNullOrEmpty(newValue))
                 return;
-            string commaSeparator = ".";
-            if (!newValue.Contains(commaSeparator))
-            {
-                Text += commaSeparator + "0";
             }
+        }
+        base.OnKeyDown(e);
+
+        if (string.IsNullOrEmpty(Text))
+            Text = 0.0f.ToString();
+
+        float newValue = float.Parse(Text, CultureInfo.CurrentCulture);
+        float? fixedVal = CheckBounds(newValue);
+        if (fixedVal != null)
+            SetValue((float)fixedVal);
+    }
+
+    protected override void OnTextInput(TextInputEventArgs e)
+    {
+        //Regex to allow only numeric input
+        if (!Regex.IsMatch(e.Text, @"^[0-9]*$")
+            || !float.TryParse(Text + e.Text, out var num)
+            )
+        {
+            e.Handled = true;
+        }
+        base.OnTextInput(e);
+
+        float newValue = float.Parse(Text);
+        float? fixedVal = CheckBounds(newValue);
+        if (fixedVal != null)
+            SetValue((float)fixedVal);
+    }
+
+    private void SetValue(float fixedVal)
+    {
+        string commaSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        string result = fixedVal.ToString();
+        if (!result.Contains(commaSeparator))
+        {
+            result += commaSeparator + "0";
+        }
+        Text = result;
+    }
+
+    private float? CheckBounds(float newValue)
+    {
+        if (newValue > Max)
+            return Max;
+        if (newValue < Min)
+            return Min;
+        return null;
+    }
+
+    private void OnTextChanged(string newValue)
+    {
+        if (string.IsNullOrEmpty(newValue))
+            return;
+
+        string commaSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        if (!newValue.Contains(commaSeparator))
+        {
+            Text += commaSeparator + "0";
         }
     }
 }
+
