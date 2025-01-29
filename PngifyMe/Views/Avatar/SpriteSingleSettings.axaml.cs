beaf41a216117f18611aeca4f338ab0ff278eed0
AWAS666 <AWAS6@protonmail.com>
2025-01-29 21:37:43 +0100

    feat: load new sprite image from file

--- PngifyMe/Views/Avatar/SpriteSingleSettings.axaml.cs
+++ PngifyMe/Views/Avatar/SpriteSingleSettings.axaml.cs
@@ -2,6 +2,9 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
 using PngifyMe.Services.CharacterSetup.Advanced;
+using PngifyMe.ViewModels.Helper;
+using System.Linq;
+using System.Net;
 
 namespace PngifyMe.Views.Avatar;
 
@@ -10,18 +13,27 @@ public partial class SpriteSingleSettings : UserControl
     public SpriteSingleSettings()
     {
         InitializeComponent();
-    }   
+    }
 
     private IStorageProvider GetStorage()
     {
         var topLevel = TopLevel.GetTopLevel(this);
         return topLevel.StorageProvider;
-    }   
+    }
 
-    private void SwapImage(object sender, RoutedEventArgs e)
+    private async void SwapImage(object sender, RoutedEventArgs e)
     {
         var con = (SpriteImage)DataContext;
-        string path = ""; // todo load path
-        con.SwitchImage(path);
+        var storage = TopLevel.GetTopLevel(this).StorageProvider;
+        var path = await storage.OpenFilePickerAsync(new FilePickerOpenOptions()
+        {
+            Title = "Select an Image",
+            FileTypeFilter = new[] { FilePickers.ImageAll },
+            AllowMultiple = false
+        });
+        if (!string.IsNullOrEmpty(path.FirstOrDefault()?.Path?.AbsolutePath))
+        {
+            con.SwitchImage(WebUtility.UrlDecode(path.FirstOrDefault()?.Path?.AbsolutePath));
+        }
     }
 }
