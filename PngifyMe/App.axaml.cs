6b9458650d84737889f6376ca6a6bc1bf1bccec9
AWAS666 <AWAS6@protonmail.com>
2025-03-21 12:25:49 +0100

    feat: splash screen

--- PngifyMe/App.axaml.cs
+++ PngifyMe/App.axaml.cs
@@ -1,25 +1,26 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using CppSharp.Types.Std;
-using PngifyMe.Helpers;
+using Avalonia.Threading;
 using PngifyMe.Services;
 using PngifyMe.Services.Hotkey;
 using PngifyMe.Services.Twitch;
 using PngifyMe.Services.WebSocket;
 using PngifyMe.Steam;
+using PngifyMe.ViewModels;
 using PngifyMe.Views;
 using Serilog;
-using Serilog.Events;
 using System;
-using System.Globalization;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace PngifyMe;
 
 public partial class App : Application
 {
+    private MainWindow _mainWindow;
+    private SplashScreen _splashScreenWindow;
+    private SplashScreenViewModel _splashVM;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -27,41 +28,70 @@ public partial class App : Application
 
     public override async void OnFrameworkInitializationCompleted()
     {
+        TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+        {
+            // Open splash screen as early as possible
+            _splashScreenWindow = new SplashScreen();
+            _splashVM = new SplashScreenViewModel();
+            _splashScreenWindow.DataContext = _splashVM;
+            desktopLifetime.MainWindow = _splashScreenWindow;
+
+            _splashScreenWindow.Show();
+            await UpdateSplash("Loading Settings");
+            await SettingsManager.LoadAsync();
+
+            await UpdateSplash("Starting app");
+
+            Dispatcher.UIThread.Post(async () => await CompleteApplicationStart(), DispatcherPriority.Background);
+            //await CompleteApplicationStart();
+        }
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    public async Task CompleteApplicationStart()
+    {
         // load language here, either system or steam
         Lang.Resources.Culture = SteamLocalization.GetStartUpCulture();
+        await UpdateSplash("Starting audio");
         AudioService.Init();
+        await UpdateSplash("Starting Websocket Server");
         WebSocketServer.Start();
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            _mainWindow = new MainWindow();
             HotkeyManager.Start(desktop);
-            desktop.MainWindow = new MainWindow
-            {
-            };
-            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
             if (SettingsManager.Current.Twitch.Enabled == true)
+            {
+                await UpdateSplash("Connecting to twitch");
                 await TwitchEventSocket.Start();
+            }
 
             if (OperatingSystem.IsWindows())
             {
+                await UpdateSplash("Init Spout");
                 SpoutRenderer.Init();
             }
 
-        }
-        //else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        //{
-        //    singleViewPlatform.MainView = new MainView
-        //    {
-        //        DataContext = new MainViewModel()
-        //    };
-        //}
+            desktop.MainWindow = _mainWindow;
+            await UpdateSplash("Finishing");
+            _mainWindow.Show();
 
-        base.OnFrameworkInitializationCompleted();
+            // close splashscreen
+            _splashScreenWindow.Close();
+        }
+    }
+
+    public async Task UpdateSplash(string text)
+    {
+        _splashVM.Text = text;
+        await Task.Delay(1); // needed for ui to update
     }
 
     private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
     {
         Log.Fatal($"Task error: {e.Exception.Message}");
-    }    
+    }
 }
---
+++ PngifyMe/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PngifyMe.ViewModels;
+
+public partial class SplashScreenViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string text = "Loading...";
+}
